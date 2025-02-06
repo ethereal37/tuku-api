4c8471b813d37c0a429bc7c9a0025c68e27fd6b5
ethereal37 <134152410+ethereal37@users.noreply.github.com>
2025-02-06 19:59:51 +0800

    img.php

--- img.php
+++ img.php
@@ -1,6 +1,6 @@
 <?php
 $seed =time();
 $num = rand(1, 21);
-$picpath = "https://tuku.ethereal.ac.cn/img/".$num.".jpg";
+$picpath = "https://github.com/ethereal37/tuku-api/tree/main/img".$num.".jpg";
 die(header("Location: $picpath"));
 ?>
